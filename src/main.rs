1eceb29006e7b676bee4a2d71ae9f25cf9000365
shockham <samuel.hockham@gmail.com>
2017-04-13 14:08:09 +0100

    Add usage and arguments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ extern crate hyper;
 
 mod retrieve;
 
+use std::env;
+
+fn usage() {
+    println!("usage: hansard [-h | --help] <command>");
+    println!("  commons Grabs the last 20 commons bound volumes");
+    println!("  help    Displays this message");
+}
+
 fn main() {
-    retrieve::retrieve();
+    let arg = env::args().nth(1).unwrap_or("".to_string());
+
+    match arg.as_str() {
+        "commons" => retrieve::retrieve(),
+        "help" | "-h" | "--help" | _ => usage(),
+    }
 }
