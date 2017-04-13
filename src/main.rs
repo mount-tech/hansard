d3ed222be64aa6cc0b53c05e8ebd8109cf43131f
shockham <samuel.hockham@gmail.com>
2017-04-13 13:51:17 +0100

    Move retrieval into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,8 @@
 extern crate atom_syndication;
 extern crate hyper;
 
-use atom_syndication::Feed;
-use hyper::Client;
-use std::io::Read;
-use std::thread;
-
-use std::fs::File;
-use std::io::prelude::*;
-
-const BOUND_VOL_URL: &'static str = "http://api.data.parliament.uk/resources/files/feed?dataset=14";
-const BASE: &'static str = "./data/";
-
-fn get_save_zip(url: String) -> thread::JoinHandle<()> {
-    thread::spawn(move || {
-        let mut zip_buf = Vec::new();
-
-        println!("Getting: {}", url);
-
-        if let Err(e) =  Client::new()
-            .get(url.as_str())
-            .send().unwrap()
-            .read_to_end(&mut zip_buf) {
-            
-            println!("Error: {:?}", e);
-            return;
-        }
-
-        let split_path = url.split("/").collect::<Vec<&str>>();
-        let file_name = split_path.last().unwrap();
-
-        println!("Saving:{}", file_name);
-
-        let mut file = File::create(format!("{}/{}", BASE, file_name)).unwrap();
-        file.write_all(zip_buf.as_slice()).unwrap(); 
-    })
-}
+mod retrieve;
 
 fn main() {
-    let mut atom_str = String::new();
-
-    Client::new()
-        .get(BOUND_VOL_URL)
-        .send().unwrap()
-        .read_to_string(&mut atom_str).unwrap();
-
-    let feed = atom_str.parse::<Feed>().unwrap();
-
-    if let Err(e) = std::fs::create_dir(BASE) {
-        println!("{}", e);
-    }
-
-    let vol_urls = feed.entries.iter()
-        .map(|e| e.links.first().unwrap().href.clone())
-        .collect::<Vec<String>>();
-
-    let handles = vol_urls.iter()
-        .map(|url| get_save_zip(url.clone()))
-        .collect::<Vec<thread::JoinHandle<()>>>();
-
-    for h in handles {
-        h.join().unwrap();
-    }
+    retrieve::retrieve();
 }
